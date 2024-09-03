4cdefc5ca766ea9284b039d35c78149357f54b38
alexfedsv <alfedsv@yandex.ru>
2024-09-03 16:20:48 +0300

    initial commit

---
+++ miniapp/miniapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  miniapp
+//
+//  Created by  Alexander Fedoseev on 03.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
